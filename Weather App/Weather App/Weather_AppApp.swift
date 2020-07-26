85f2889c9ece68d5181edbd8b06d452bafc5026d
Augusto Ramos <ramos.augusto94@gmail.com>
2020-07-26 11:56:52 -0300

    create initial project configuration

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Augusto Ramos on 26/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
